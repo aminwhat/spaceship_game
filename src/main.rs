6e34be6cb1b0820f67589bf0dccd4cdcab157f18
aminwhat <m.aminwhat@gmail.com>
2025-01-01 12:37:19 +0330

    unknown commit

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,18 @@
-use bevy::prelude::*;
+mod debug;
+mod movement;
+mod spaceship;
 
-#[derive(Component, Debug)]
-pub struct Velocity {
-    pub value: Vec3,
-}
+use bevy::prelude::*;
+use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceship::SpaceshipPlugin;
 
 fn main() {
     App::new()
-        .add_systems(Startup, spawn_spaceship)
-        .add_systems(Update, (update_position, print_position))
+        .add_plugins(DebugPlugin)
+        //* User Configured Plugins */
+        .add_plugins(SpaceshipPlugin)
         .add_plugins(DefaultPlugins)
+        .add_plugins(MovementPlugin)
         .run();
-}
-
-fn spawn_spaceship(mut commands: Commands) {
-    commands.spawn((
-        SpatialBundle::default(),
-        Velocity {
-            value: Vec3 {
-                x: 0.,
-                y: 0.,
-                z: 0.,
-            },
-        },
-    ));
-}
-
-fn update_position(mut query: Query<(&Velocity, &mut Transform)>) {
-    for (velocity, mut transform) in query.iter_mut() {
-        transform.translation.x += velocity.value.x;
-        transform.translation.y += velocity.value.y;
-        transform.translation.z += velocity.value.z;
-    }
-}
-
-fn print_position(query: Query<(Entity, &Transform)>) {
-    for (entity, transform) in query.iter() {
-        info!(
-            "Entity {:?} is at position {:?}",
-            entity, transform.translation
-        );
-    }
 }
